6e0de48a573e29d160bce912cc523640f1b82c53
John Topley <john.topley@ons.gov.uk>
2020-05-06 16:16:12 +0100

    Added Response type for returned JSON

--- agent/cmd/dashboardagent/main.go
+++ agent/cmd/dashboardagent/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 
 	googlecloud "github.com/ONSdigital/gcp-projects-dashboard/agent/pkg/googlecloud"
+	response "github.com/ONSdigital/gcp-projects-dashboard/agent/pkg/response"
 )
 
 func main() {
@@ -12,5 +15,33 @@ func main() {
 	clusters := client.ListClusters()
 	cluster := client.GetCluster(clusters.Clusters[0].Name)
 
-	fmt.Printf("Cluster %s within project %s has master version %s", cluster.Name, project.Name, cluster.CurrentMasterVersion)
+	projectJSON := response.Project{
+		ID:         project.ProjectId,
+		Labels:     project.Labels,
+		Name:       project.Name,
+		Number:     project.ProjectNumber,
+		CreateTime: project.CreateTime}
+
+	clusterJSON := response.Cluster{
+		Name:                  cluster.Name,
+		Description:           cluster.Description,
+		CreateTime:            cluster.CreateTime,
+		InitialClusterVersion: cluster.InitialClusterVersion,
+		CurrentMasterVersion:  cluster.CurrentMasterVersion,
+		CurrentNodeVersion:    cluster.CurrentNodeVersion,
+		InitialNodeCount:      cluster.InitialNodeCount,
+		CurrentNodeCount:      cluster.CurrentNodeCount,
+		MaximumPodsPerNode:    cluster.DefaultMaxPodsConstraint.MaxPodsPerNode,
+		LoggingService:        cluster.LoggingService,
+		MonitoringService:     cluster.MonitoringService,
+		DatabaseEncryption:    cluster.DatabaseEncryption.State,
+		DatabaseEncryptionKey: cluster.DatabaseEncryption.KeyName}
+
+	responseJSON := response.Response{Project: projectJSON, Cluster: clusterJSON}
+
+	response, err := json.MarshalIndent(responseJSON, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", response)
 }
---
+++ agent/pkg/response/response.go
@@ -0,0 +1,36 @@
+package response
+
+type (
+
+	// Response represents a JSON response containing GCP project and GKE cluster details.
+	Response struct {
+		Project Project
+		Cluster Cluster
+	}
+
+	// Project represents a GCP project.
+	Project struct {
+		ID         string            `json:"id,omitempty"`
+		Labels     map[string]string `json:"labels,omitempty"`
+		Name       string            `json:"name,omitempty"`
+		Number     int64             `json:"number,omitempty,string"`
+		CreateTime string            `json:"createTime,omitempty"`
+	}
+
+	// Cluster represents a GKE cluster.
+	Cluster struct {
+		Name                  string `json:"name,omitempty"`
+		Description           string `json:"description,omitempty"`
+		CreateTime            string `json:"createTime,omitempty"`
+		InitialClusterVersion string `json:"initialClusterVersion,omitempty"`
+		CurrentMasterVersion  string `json:"currentMasterVersion,omitempty"`
+		CurrentNodeVersion    string `json:"currentNodeVersion,omitempty"`
+		InitialNodeCount      int64  `json:"initialNodeCount,omitempty"`
+		CurrentNodeCount      int64  `json:"currentNodeCount,omitempty"`
+		MaximumPodsPerNode    int64  `json:"maximumPodsPerNode,omitempty"`
+		LoggingService        string `json:"loggingService,omitempty"`
+		MonitoringService     string `json:"monitoringService,omitempty"`
+		DatabaseEncryption    string `json:"databaseEncryption,omitempty"`
+		DatabaseEncryptionKey string `json:"databaseEncryptionKey,omitempty"`
+	}
+)
