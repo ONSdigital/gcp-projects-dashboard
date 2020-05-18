97a02b560c21bab9544cb87970d11c854efa142f
John Topley <john.topley@ons.gov.uk>
2020-05-18 17:23:14 +0100

    Initial integration with Firestore

--- webapp/app.rb
+++ webapp/app.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+require 'logger'
 require 'sinatra'
+require 'google/cloud/firestore'
 
 before do
   headers 'Content-Type' => 'text/html; charset=utf-8'
@@ -9,5 +11,14 @@ before do
 end
 
 get '/?' do
+  logger = Logger.new($stdout)
+  firestore_project = ENV['FIRESTORE_PROJECT']
+  Google::Cloud::Firestore.configure { |config| config.project_id = firestore_project }
+  firestore_client = Google::Cloud::Firestore.new
+  projects_col = firestore_client.col('gcp-projects-dashboard')
+  projects_col.get do |project|
+    logger.info "#{project.document_id} was last updated at #{project[:updated]}."
+  end
+
   erb :index, locals: { title: 'GCP Projects Dashboard' }
 end
