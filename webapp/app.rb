91565c7f1e57ce7671909f0d53c08e306d9186ba
John Topley <john.topley@ons.gov.uk>
2020-05-19 14:42:34 +0100

    Dashboard now displays initial cluster info

--- webapp/app.rb
+++ webapp/app.rb
@@ -4,6 +4,16 @@ require 'logger'
 require 'sinatra'
 require 'google/cloud/firestore'
 
+helpers do
+  def d(text)
+    Time.parse(text).utc.strftime('%A %d %b %Y %H:%M UTC')
+  end
+
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
 before do
   headers 'Content-Type' => 'text/html; charset=utf-8'
   user_header = request.env['HTTP_X_GOOG_AUTHENTICATED_USER_EMAIL']
@@ -11,14 +21,10 @@ before do
 end
 
 get '/?' do
-  logger = Logger.new($stdout)
   firestore_project = ENV['FIRESTORE_PROJECT']
   Google::Cloud::Firestore.configure { |config| config.project_id = firestore_project }
   firestore_client = Google::Cloud::Firestore.new
-  projects_col = firestore_client.col('gcp-projects-dashboard')
-  projects_col.get do |project|
-    logger.info "#{project.document_id} was last updated at #{project[:updated]}."
-  end
+  projects = firestore_client.col('gcp-projects-dashboard').list_documents
 
-  erb :index, locals: { title: 'GCP Projects Dashboard' }
+  erb :index, locals: { title: 'GCP Projects Dashboard', projects: projects }
 end
