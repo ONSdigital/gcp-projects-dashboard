fd2b7df4d73507af1c2603ae6e9901f90ac0e631
John Topley <john.topley@ons.gov.uk>
2020-06-03 08:19:47 +0100

    Appending a random integer to defeat image caching

--- webapp/public/javascripts/app.js
+++ webapp/public/javascripts/app.js
@@ -2,11 +2,11 @@ $(function() {
   $(".bookmark").on( "click", function(event) {
     if ($(event.target).attr("src") == "/svg/bookmark-off.svg") {
       $.post("/addbookmark", { bookmark: event.target.id });
-      $(event.target).attr("src", "/svg/bookmark-on.svg?rand=" + Math.random());
+      $(event.target).attr("src", "/svg/bookmark-on.svg?r=" + Math.random());
       $(event.target).attr("alt", "Project bookmarked");
     } else {
       $.post("/removebookmark", { bookmark: event.target.id });
-      $(event.target).attr("src", "/svg/bookmark-off.svg?rand=" + Math.random());
+      $(event.target).attr("src", "/svg/bookmark-off.svg?r=" + Math.random());
       $(event.target).attr("alt", "Bookmark project");
     }
   });
